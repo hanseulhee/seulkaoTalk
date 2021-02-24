63c6f6ab0929ae722f5a1e277baef6512b6aba39
hanseulhee <3021062@gmail.com>
2021-02-24 20:19:57 +0900

    login form

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React from "react";
 import "./styles/Login.css";
 
-function Login(){
-    return  <div className="login__tool">
-        
+function Login() {
+  return (
+    <div className="login__tool">
+      <form id="form" className="form">
+        <div className="signinbox">
+          <div className="inform">Sign In</div>
+
+          <div className="form-control">
+            <input
+              type="text"
+              className="name input"
+              placeholder="Enter username"
+            />
+
+            <small>Error message</small>
+          </div>
+
+          <div className="form-control">
+            <input
+              type="password"
+              className="password input"
+              placeholder="Enter password"
+            />
+            <small>Error message</small>
+          </div>
+        </div>
+      </form>
     </div>
+  );
 }
 
 export default Login;
